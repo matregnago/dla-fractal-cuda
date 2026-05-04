3c0951b52b26def96ff67711e28fa36530f1e7b3
Matheus Tregnago <maugustotreg@gmail.com>
2026-05-04 11:08:43 -0300

    feat: nix shell e imagens em png

---
+++ shell.nix
@@ -0,0 +1,15 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+
+pkgs.mkShell {
+  buildInputs = [
+    (pkgs.python3.withPackages (
+      ps: with ps; [
+        numpy
+        matplotlib
+        pandas
+      ]
+    ))
+  ];
+}
